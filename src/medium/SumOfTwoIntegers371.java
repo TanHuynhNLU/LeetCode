6be434ed1742a3ee954b4573a02b8d212864ecb4
TanHuynhNLU <hntan2000@gmail.com>
2025-06-24 13:39:27 +0700

    371. Sum of Two Integers

---
+++ src/medium/SumOfTwoIntegers371.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class SumOfTwoIntegers371 {
+//    public int getSum(int a, int b) {
+//        return (a ^ b) + ((a & b) << 1);
+//    }
+
+//    public int getSum(int a, int b) {
+//        while(b != 0){
+//            int carry = (a & b) << 1;
+//            a = a ^ b;
+//            b = carry;
+//        }
+//        return a;
+//    }
+}
